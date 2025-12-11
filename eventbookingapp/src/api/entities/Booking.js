99fb9c51116a62226f4f5f367ec0211424560057
hoppedlight <61391150+hoppedlight@users.noreply.github.com>
2025-12-11 15:53:36 +0100

    Deleted previous node.js elements

--- eventbookingapp/src/api/entities/Booking.js
+++
@@ -1,5 +0,0 @@
-import { api } from "../client";
-
-export const Booking = {
-  create: api.bookings.create,
-};
--- eventbookingapp/src/api/entities/Event.js
+++
@@ -1,9 +0,0 @@
-import { api } from "../client";
-
-export const Event = {
-  list: api.events.list,
-  filter: api.events.filter,
-  create: api.events.create,
-  update: api.events.update,
-  delete: api.events.delete,
-};
