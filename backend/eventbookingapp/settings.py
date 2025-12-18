2af5b447c56541ff423caa9b2b741401f34a2729
hoppedlight <61391150+hoppedlight@users.noreply.github.com>
2025-12-18 13:26:32 +0100

    changed the time for auth

--- backend/eventbookingapp/settings.py
+++ backend/eventbookingapp/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=99999),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
