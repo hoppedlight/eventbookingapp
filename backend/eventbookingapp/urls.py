77ca433ae998787ddd2e4d1a6c36e1a01b34eccd
Dmytro Mytsko <d.mytsko@nltu.lviv.ua>
2026-02-11 12:41:25 +0200

    added function to get bookings

--- backend/eventbookingapp/urls.py
+++ backend/eventbookingapp/urls.py
@@ -14,26 +14,35 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from backend.views import login_view, register_view, get_current_user, update_current_user, fetch_events, create_booking, create_event, upload_file, delete_event
+from backend.views import (
+    login_view,
+    register_view,
+    get_current_user,
+    update_current_user,
+    fetch_events,
+    create_booking,
+    create_event,
+    upload_file,
+    delete_event,
+)
 
 urlpatterns = [
-    path('api/register/', register_view),
-    path('api/login/', login_view),
-    path('api/me/', get_current_user),
-    path('api/me/update/', update_current_user),
-    path('api/events/', fetch_events),
+    path("api/register/", register_view),
+    path("api/login/", login_view),
+    path("api/me/", get_current_user),
+    path("api/me/update/", update_current_user),
+    path("api/events/", fetch_events),
     path("api/events/create/", create_event),
     path("api/events/delete/<str:event_id>/", delete_event, name="delete_event"),
-    path('api/bookings/', create_booking),
+    path("api/bookings/", create_booking),
+    path("api/bookings/get/", get_user_bookings),
     path("api/upload/", upload_file, name="upload-file"),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
